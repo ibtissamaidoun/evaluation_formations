8d76ab3f65202f51f2ad22dc6e626e6cc59cab27
safaa <safaa.bouhnine@etu.uae.ac.ma>
2025-05-02 16:55:41 +0100

    sidebar

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
+import { withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+      provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+      provideHttpClient(withFetch()),
+      provideAnimationsAsync(),
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+  ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,24 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-import { ResetComponent } from './features/reset/reset.component'; // Importer ton composant
+// import { ResetComponent } from './features/reset/reset.component'; // Importer ton composant
+import { Dashboard } from './features/dashboard/dashboard';
+import { AppLayout } from './layout/component/app.layout';
 
-export const routes: Routes = [
-  { path: 'reset', component: ResetComponent }, // Ta route de reset
-  // d'autres routes ici
+  export const appRoutes: Routes = [
+    {
+      // path: 'reset', component: ResetComponent ,
+      path: '',  component: AppLayout,
+        children: [
+            { path: 'dashboard', component: Dashboard },
+            // { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+            // { path: 'documentation', component: Documentation },
+            // { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+        ]
+    },
+    // { path: 'landing', component: Landing },
+    // { path: 'notfound', component: Notfound },
+    // { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    { path: '**', redirectTo: '/notfound' }
 ];
+
