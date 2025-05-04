89c8a68efb14c85fe0c71ab5e90bb08ec77bc34d
MeryemeBoussaid <boussaid.meryeme@etu.uae.ac.ma>
2025-05-04 13:06:48 +0100

    nav

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9702684b19850d5c3ca57f32b14d5a68e27dfdbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678a1fa7b505810860a19fb1ab9937eb1dae98d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
